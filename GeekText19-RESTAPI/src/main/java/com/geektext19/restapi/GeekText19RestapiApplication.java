30e03862f49758c8f459398717cc51e155767de3
Nicc727 <142936406+Nicc727@users.noreply.github.com>
2023-10-06 18:54:29 -0400

    Add files via upload

---
+++ GeekText19-RESTAPI/src/main/java/com/geektext19/restapi/GeekText19RestapiApplication.java
@@ -0,0 +1,13 @@
+package com.geektext19.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekText19RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekText19RestapiApplication.class, args);
+	}
+
+}
