e0c7d0ee04fd5e2f4be4966a6ac576c6131f6907
Matthew2906 <104445973+Matthew2906@users.noreply.github.com>
2023-10-16 17:48:46 -0400

    Update bookdetails.js

--- bookdetails.js
+++ bookdetails.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // MySQL database connection configuration
+
 const dbConfig = {
   host: 'localhost',
   user: 'team19admin',
@@ -32,7 +33,7 @@ app.post('/books', async (req, res) => {
     }
 });
 
-// retrieves a book by ISBN
+ // gets books by ISBN
 app.get('/books/:isbn', async (req, res) => {
     const isbn = req.params.isbn;
     try {
@@ -45,33 +46,6 @@ app.get('/books/:isbn', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).send('Error fetching book.');
-    }
-});
-
-// creates an author
-app.post('/authors', async (req, res) => {
-    const author = req.body;
-    try {
-        const [result] = await pool.execute(
-            `INSERT INTO authors (firstName, lastName, biography, publisher) VALUES (?, ?, ?, ?)`,
-            [author.firstName, author.lastName, author.biography, author.publisher]
-        );
-        res.status(201).send('Author added successfully.');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error adding author.');
-    }
-});
-
-// Route to retrieve books by author ID
-app.get('/authors/:authorId/books', async (req, res) => {
-    const authorId = req.params.authorId;
-    try {
-        const [rows] = await pool.query(`SELECT * FROM books WHERE authorId = ?`, [authorId]);
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error fetching books.');
     }
 });
 
