7ec2ab7e8d4c2e4cdfe76ad492f7e1fb8f8b7b70
lquincoso <122310652+lquincoso@users.noreply.github.com>
2023-10-23 19:52:45 -0400

    Changes for CreditCard in general

--- src/main/java/com/geektext19/restapi/controllers/profile_management/requests/CreateUserRequest.java
+++ src/main/java/com/geektext19/restapi/controllers/profile_management/requests/CreateUserRequest.java
@@ -3,22 +3,17 @@ package com.geektext19.restapi.controllers.profile_management.requests;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CreateUserRequest {
+public class CreateUserRequest extends BaseUserRequest {
     private String username;
-    private String password;
-    private String fullName;
     private String email;
-    private String address;
 
     public CreateUserRequest() {
     }
 
     public CreateUserRequest(String username, String password, String fullName, String email, String address) {
+        super(password, fullName, address);
         this.username = username;
-        this.password = password;
-        this.fullName = fullName;
         this.email = email;
-        this.address = address;
     }
 
     public String getUsername() {
@@ -29,35 +24,11 @@ public class CreateUserRequest {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 }
--- src/main/java/com/geektext19/restapi/entities/CreditCard.java
+++ src/main/java/com/geektext19/restapi/entities/CreditCard.java
@@ -1,4 +1,65 @@
 package com.geektext19.restapi.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class CreditCard {
+    @Id
+    private String creditCardNumber;
+    private String cardholderName;
+    private String expirationDate;
+    private String CVV;
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    private User user;
+    public CreditCard() {
+    }
+
+    public CreditCard(String creditCardNumber, String cardholderName, String expirationDate, String CVV, User user) {
+        this.creditCardNumber = creditCardNumber;
+        this.cardholderName = cardholderName;
+        this.expirationDate = expirationDate;
+        this.CVV = CVV;
+        this.user = user;
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public String getCardholderName() {
+        return cardholderName;
+    }
+
+    public void setCardholderName(String cardholderName) {
+        this.cardholderName = cardholderName;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCVV() {
+        return CVV;
+    }
+
+    public void setCVV(String CVV) {
+        this.CVV = CVV;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
