b7d32b7deb8ef371886a9c816a698558561a1691
lquincoso <122310652+lquincoso@users.noreply.github.com>
2023-11-06 12:23:11 -0500

    Created an http exception handler

---
+++ src/main/java/com/geektext19/restapi/controllers/HttpExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.geektext19.restapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class HttpExceptionHandler {
+  
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return ResponseEntity.badRequest().body(errors);
+  }
+}
